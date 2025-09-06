6254ef09ff1fb5edd72e004b9b21f684d8ea8121
WalterJonas <jonasdowalter@gmail.com>
2025-09-06 20:34:37 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    //arquivo principal
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello World!</h1>
+        <p>Nova Feature</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
